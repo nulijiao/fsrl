1b7dc10150106fa8ef4c7b6cfbc985de3498d514
baiyuejiao <baiyuejiao@zhihu.com>
2019-04-11 13:59:43 +0800

    commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import './pages/home_page.dart';
+
+import './pages/Articles/ArticleList.dart';
+
+void main() => runApp(new MaterialApp(
+  home: new HomePage(),
+  routes: <String, WidgetBuilder> {
+      '/a': (BuildContext context) => new Example01(),
+    },
+  )
+);
+
